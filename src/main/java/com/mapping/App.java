b3566fa49ba8286a200637495f5bc0f6cb847c38
Vishal Vahatule <vwahatule7@gmail.com>
2025-07-24 01:22:15 +0530

    Add main class to persist Questions and Answers with ManyToMany mapping.

--- src/main/java/com/mapping/App.java
+++ src/main/java/com/mapping/App.java
@@ -1,8 +1,14 @@
 package com.mapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import com.mapping.Entity.Answer;
+import com.mapping.Entity.Question;
 import com.mapping.Utility.HibernateUtility;
 
 
@@ -12,6 +18,32 @@ public class App
     public static void main( String[] args )
     {
         SessionFactory factory = HibernateUtility.getSessionFactory();
-        Session session
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        Question q1 = new Question("Where do you live?");
+        Question q2 = new Question("What is your location");
+   
+        Answer a1 = new Answer("I live in Maharashtra");
+        Answer a2 = new Answer("I live in pune");
+        Answer a3 = new Answer("I live in india");
+        
+        List<Answer> aList = Arrays.asList(a1,a2,a3);
+        
+        q1.setAnswerList(aList);
+        q2.setAnswerList(aList);
+        session.persist(a1);
+        session.persist(a2);
+        session.persist(a3);
+        
+        session.persist(q1);
+        session.persist(q2);
+        
+        
+        
+        transaction.commit();
+        session.close();
+        
+        
     }
 }
